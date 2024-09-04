118cdb8a9fa70e6bf9382b40f5c2f2dfc648be61
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-09-04 19:45:13 +0600

    foods and electronics pages complete

--- Route/PublicRouter/publicRoute.js
+++ Route/PublicRouter/publicRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const publicRouter = express.Router();
+const { ObjectId } = require('mongodb');
 
 // Middleware to log requests
 publicRouter.use((req, res, next) => {
@@ -8,16 +9,81 @@ publicRouter.use((req, res, next) => {
 });
 
 // GET request to fetch all foods
-publicRouter.get('/', async (req, res) => {
+publicRouter.get('/food', async (req, res) => {
   try {
     const foodsCollection = req.app.locals.db.collection("Foods");
     const result = await foodsCollection.find().toArray();
-    console.log(result);
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch foods', details: error.message });
   }
 });
+
+//get data with id
+publicRouter.get('/food/:id', async (req, res) => {
+  try {
+    const foodsCollection = req.app.locals.db.collection("Foods");
+
+    const id = req.params.id;
+    console.log(id);
+
+    // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
+    const filter = { _id: new ObjectId(id) };
+
+    const result = await foodsCollection.findOne(filter);
+    console.log(result);
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: 'Food not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch food', details: error.message });
+  }
+});
+
+
+
+//get data from electronics collection
+publicRouter.get('/electronics', async (req, res) => {
+  try {
+    const electronicsCollection = req.app.locals.db.collection("Electronics");
+    const result = await electronicsCollection.find().toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch Electronics', details: error.message });
+  }
+});
+
+
+
+//get data from electronics collection with id
+publicRouter.get('/electronics/:id', async (req, res) => {
+  try {
+    const electronicsCollection = req.app.locals.db.collection("Electronics");
+
+    const id = req.params.id;
+    console.log(id);
+
+    // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
+    const filter = { _id: new ObjectId(id) };
+
+    const result = await electronicsCollection.findOne(filter);
+    console.log(result);
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: 'Food not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch food', details: error.message });
+  }
+});
+
+
+
 
 
 

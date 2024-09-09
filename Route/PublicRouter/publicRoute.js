14fb15f41496041ffa419f98df59e3a6b5d23261
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-09-10 00:36:09 +0600

    regular section api complete

--- Route/PublicRouter/publicRoute.js
+++ Route/PublicRouter/publicRoute.js
@@ -25,13 +25,13 @@ publicRouter.get('/food/:id', async (req, res) => {
     const foodsCollection = req.app.locals.db.collection("Foods");
 
     const id = req.params.id;
-    console.log(id);
+
 
     // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
     const filter = { _id: new ObjectId(id) };
 
     const result = await foodsCollection.findOne(filter);
-    console.log(result);
+ 
 
     if (result) {
       res.status(200).json(result);
@@ -64,13 +64,12 @@ publicRouter.get('/electronics/:id', async (req, res) => {
     const electronicsCollection = req.app.locals.db.collection("Electronics");
 
     const id = req.params.id;
-    console.log(id);
-
+ 
     // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
     const filter = { _id: new ObjectId(id) };
 
     const result = await electronicsCollection.findOne(filter);
-    console.log(result);
+
 
     if (result) {
       res.status(200).json(result);
@@ -101,13 +100,13 @@ publicRouter.get('/beauty/:id', async (req, res) => {
     const beautyCollection = req.app.locals.db.collection("beauty");
 
     const id = req.params.id;
-    console.log(id);
+
 
     // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
     const filter = { _id: new ObjectId(id) };
 
     const result = await beautyCollection.findOne(filter);
-    console.log(result);
+  
 
     if (result) {
       res.status(200).json(result);
@@ -139,13 +138,13 @@ publicRouter.get('/health/:id', async (req, res) => {
     const healthCollection = req.app.locals.db.collection("health");
 
     const id = req.params.id;
-    console.log(id);
+
 
     // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
     const filter = { _id: new ObjectId(id) };
 
     const result = await healthCollection.findOne(filter);
-    console.log(result);
+   
 
     if (result) {
       res.status(200).json(result);
@@ -159,22 +158,129 @@ publicRouter.get('/health/:id', async (req, res) => {
 
 
 
-// PATCH request
-publicRouter.patch('/', async (req, res) => {
-  res.send("PATCH");
+
+//get data from Telivision collection
+publicRouter.get('/telivision', async (req, res) => {
+  try {
+    const telivisionCollection = req.app.locals.db.collection("Telivision");
+    const result = await telivisionCollection.find().toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch Electronics', details: error.message });
+  }
 });
 
-// PUT request
-publicRouter.put('/', async (req, res) => {
-  res.send("PUT");
+
+//get data from Telivision collection with id
+publicRouter.get('/telivision/:id', async (req, res) => {
+  try {
+    const telivisionCollection = req.app.locals.db.collection("Telivision");
+
+    const id = req.params.id;
+
+
+    // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
+    const filter = { _id: new ObjectId(id) };
+
+    const result = await telivisionCollection.findOne(filter);
+
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: 'Food not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch food', details: error.message });
+  }
 });
+
+
+
+//get data from Telivision collection
+publicRouter.get('/babycare', async (req, res) => {
+  try {
+    const babyCareCollection = req.app.locals.db.collection("babyCare");
+    const result = await babyCareCollection.find().toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch Electronics', details: error.message });
+  }
+});
+
+
+//get data from Telivision collection with id
+publicRouter.get('/babycare/:id', async (req, res) => {
+  try {
+    const babyCareCollection = req.app.locals.db.collection("babyCare");
+
+    const id = req.params.id;
+ 
+
+    // _id ফিল্টারের জন্য ObjectId কনভার্ট করা
+    const filter = { _id: new ObjectId(id) };
+
+    const result = await babyCareCollection.findOne(filter);
+
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: 'Food not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch food', details: error.message });
+  }
+});
+
+
+//get data from regular collection
+publicRouter.get('/regular', async (req, res) => {
+  try {
+    const regularCollection = req.app.locals.db.collection("Regular");
+    const result = await regularCollection.find().toArray();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch Electronics', details: error.message });
+  }
+});
+
+
+//get data from Telivision collection with id
+publicRouter.get('/regular/:id', async (req, res) => {
+  try {
+    const regularCollection = req.app.locals.db.collection("Regular");
+
+    const id = req.params.id;
+
+    // চেক করুন যে id একটি বৈধ ObjectId কিনা
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const filter = { _id: new ObjectId(id) };
+
+    const result = await regularCollection.findOne(filter);
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: 'Food not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch food', details: error.message });
+  }
+});
+
+
+
+
 
 // POST request to add customer data
 publicRouter.post('/', async (req, res) => {
   try {
     const customersCollection = req.app.locals.db.collection("customer");
     const customerData = req.body;
-    console.log(customerData, "hello cus");
 
     const result = await customersCollection.insertOne(customerData);
     res.status(201).json({ message: 'Customer added successfully', result });
